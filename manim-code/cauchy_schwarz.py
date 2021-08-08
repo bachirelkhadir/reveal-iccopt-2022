708cceea14baa7737bb3f9f3ac5c96740f819f7a
bachirelkhadir <bachir009+git@gmail.com>
2021-08-08 13:43:57 -0400

    manim-code/cauchy_schwarz.py

--- manim-code/cauchy_schwarz.py
+++ manim-code/cauchy_schwarz.py
@@ -3,7 +3,7 @@ exec(get_custom_config()["universal_import_line"])
 from colors import *
 import numpy as np
 from common import *
-from slide_scene import SlideScene
+from manimslide import SlideScene
 
 
 np.random.seed(0)
