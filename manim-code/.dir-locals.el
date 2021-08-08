7c39b532472d104237c3e30b81be7161e7b2f65d
bachirelkhadir <bachir009+git@gmail.com>
2021-08-08 13:20:00 -0400

    manim-code/.dir-locals.el

--- manim-code/.dir-locals.el
+++ manim-code/.dir-locals.el
@@ -1,12 +1,12 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((pyvenv-workon . "manim")
+((python-mode . ((pyvenv-workon . "manimslides")
                  (eval . (manim-mode 1))
                  (eval . (git-auto-commit-mode 1))
                  ;(eval . (setq lsp-python-ms-extra-paths ["/home/bachir/Dropbox/Youtube/common/"]))
                  ;(eval . (setq  lsp-pyright-extra-paths ["/home/bachir/Dropbox/Youtube/common/"]))
                  (eval . (message  "Local dir variables executed for python mode."))
                  ))
- (nil  . ((pyvenv-workon . "manim")
+ (nil  . ((pyvenv-workon . "manimslides")
           (eval . (git-auto-commit-mode 1)))))
