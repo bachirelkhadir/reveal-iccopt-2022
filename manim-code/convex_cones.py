b867e0d2ec2bfe5b48ed80af34ca72c3784f4617
bachirelkhadir <bachir009+git@gmail.com>
2021-08-08 14:34:24 -0400

    manim-code/convex_cones.py

--- manim-code/convex_cones.py
+++ manim-code/convex_cones.py
@@ -384,6 +384,7 @@ class ConvexCones(SlideScene):
         self.slide_break()
 
         x_robin = LabeledPolynomialDiscovery("Robinson", "1973")
+        x_robin.shift(LEFT)
         self.add(x_robin)
         self.slide_break()
 
