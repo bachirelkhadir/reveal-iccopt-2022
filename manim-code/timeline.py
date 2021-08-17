62a9d2eb8d306a230f7ebef98db68527bc8bf230
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 17:32:55 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -40,14 +40,14 @@ class Timeline(Scene):
         # green/red rects
         green_rect = Line()\
             .set_stroke(color=BABY_GREEN, width=30, opacity=.7)
-        green_endpoint= Dot(color=BABY_GREEN)
+        green_endpoint= Dot(color=BABY_GREEN).move_to(arrow.get_corner(LEFT))
         green_rect.add_updater(lambda z: z.become(Line(arrow.get_corner(RIGHT), green_endpoint.get_center()-.01*RIGHT).match_style(z)))
         self.add(green_rect, green_endpoint)
 
 
         red_rect = Line()\
             .set_stroke(color=BABY_PINK, width=30, opacity=.7)
-        red_endpoint= Dot(color=BABY_PINK).move_to(arrow.get_center(LEFt))
+        red_endpoint= Dot(color=BABY_PINK).move_to(arrow.get_corner(LEFT))
         red_rect.add_updater(lambda z: z.become(Line(arrow.get_corner(LEFT), red_endpoint.get_center()).match_style(z)))
         self.add(red_rect, red_endpoint)
 
