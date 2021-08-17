dd5a1d9ecc80340baa823daef9525286deca18e7
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 15:03:21 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 from manimlib import *
 exec(get_custom_config()["universal_import_line"])
-from align_utils import *
-from colors import *
 import numpy as np
 np.random.seed(0)
 
