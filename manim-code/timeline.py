c8a935a5c1948205bea96c78da996ec3262ff3b2
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 16:21:33 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -15,5 +15,6 @@ class Timeline(Scene):
         tick = Line().rotate(PI/2).scale(.2)
         n3 = tick.copy().shift(3*LEFT)
         n4 = n3.copy().shift(RIGHT)
-        self.add(n3, n4)
+        nhigh = n3.copy().shift(3*RIGHT)
+        self.add(n3, n4, nhigh)
         self.wait()
