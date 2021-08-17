a3e412f9cfa836c4559520dff3c7d61dd2bbc19c
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 15:03:58 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -7,6 +7,6 @@ np.random.seed(0)
 
 class Timeline(Scene):
     def construct(self):
-        arrow = Vector().scale(8)
+        arrow = Vector().scale(10)
         self.add(arrow)
         self.wait()
