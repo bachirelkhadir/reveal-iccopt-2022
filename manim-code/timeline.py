3c577cad7593c8748ab419fedce64a9713b37721
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 15:03:13 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+from manimlib import *
+exec(get_custom_config()["universal_import_line"])
+from align_utils import *
+from colors import *
+import numpy as np
+np.random.seed(0)
+
+
+class Timeline(Scene):
+    def construct(self):
+        self.add(Text("Class " + type(self).__name__))
+        self.wait()
