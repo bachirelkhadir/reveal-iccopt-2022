60718bdd734dafac421f0e0545a551d664de8746
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 16:46:02 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -23,6 +23,8 @@ class Timeline(Scene):
         hilbert_tick = Tex("]").move_to(arrow).shift(5*LEFT)
         hilbert_lab = Tex("3").next_to(hilbert_tick, UP)
         hilbert_tracker = Line().set_stroke(color=BABY_GREEN, width=10)
+        hilbert_tracker.add_updater(lambda z: z.become(Line(
+            arrow.get_corner(LEFT), hilbert_tick).match_style(z)))
         hilbert = Group(hilbert_tick, hilbert_lab)
         self.add(hilbert_tracker)
         self.play(hilbert.animate.shift(RIGHT))
