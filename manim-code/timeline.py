46102193240213d6c18976f7a6a33febd8b2b97b
bachirelkhadir <bachir009+git@gmail.com>
2021-08-17 15:05:14 -0400

    manim-code/timeline.py

--- manim-code/timeline.py
+++ manim-code/timeline.py
@@ -9,6 +9,6 @@ class Timeline(Scene):
     def construct(self):
         arrow = Vector().scale(10)
         lab_nvar = Text(r"# variables")
-        lab_nvar.next_to(arrow, UR).shift(LEFT)
+        lab_nvar.next_to(arrow, UR).shift(2*LEFT)
         self.add(arrow, lab_nvar)
         self.wait()
